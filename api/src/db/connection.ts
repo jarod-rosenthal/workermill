b5094d65b792296db71a6e2c3a861c8d8871af27
WorkerMill Epic Agent <99708900+jarod-rosenthal@users.noreply.github.com>
2026-03-20 17:21:31 +0000

    feat: Story 1 - Database Migration + PushSubscription Entity + Connection Registration
    Story: S1
    Co-Authored-By: Backend Developer <99708900+jarod-rosenthal@users.noreply.github.com>

--- api/src/db/connection.ts
+++ api/src/db/connection.ts
@@ -280,7 +280,7 @@ import { AddBlockOnLintErrors1742700000000 } from "./migrations/1742700000000-Ad
 import { AddBlockOnE2EFailures1742800000000 } from "./migrations/1742800000000-AddBlockOnE2EFailures.js";
 import { AddApiKeyPrefixToRemoteAgents1742900000000 } from "./migrations/1742900000000-AddApiKeyPrefixToRemoteAgents.js";
 import { AddTicketKeyToKbCards1743000000000 } from "./migrations/1743000000000-AddTicketKeyToKbCards.js";
-import { AddPushSubscriptions1741200000000 } from "./migrations/1741200000000-AddPushSubscriptions.js";
+import { AddPushSubscriptions1741200000001 } from "./migrations/1741200000001-AddPushSubscriptions.js";
 import { logger } from "../utils/logger.js";
 import { OrganizationEncryptionSubscriber } from "./subscribers/OrganizationEncryptionSubscriber.js";
 import { WebhookEndpointEncryptionSubscriber } from "./subscribers/WebhookEndpointEncryptionSubscriber.js";
@@ -580,7 +580,7 @@ export const AppDataSource = new DataSource({
     EncryptExistingApiKeys1740900000000,
     AddOrgAiGuidelines1741000000000,
     AddCloudComputeBilling1741100000000,
-    AddPushSubscriptions1741200000000,
+    AddPushSubscriptions1741200000001,
     AddMaxTargetFiles1741200000000,
     BlockOnTestFailuresDefault1741300000000,
     AddGithubAppInstallationId1741400000000,
